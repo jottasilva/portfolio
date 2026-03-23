6e1a39e0091ccb0ec510c221b8ea08ef73fbcfa2
Jefferson Silva <jefferson_jsp@hotmail.com>
2026-03-23 11:02:26 -0300

    fix(images): configura remotePatterns para o Supabase

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'supabase.ogerente.site',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   async headers() {
     return [
       {
